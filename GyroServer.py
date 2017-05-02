039f647b2eb29317e24c024510959678e82bfd97
Taiwaninja <gokosss@hotmail.com>
2017-05-02 14:24:06 +0300

    Added newest changes, now works with graphs

--- GyroServer.py
+++ GyroServer.py
@@ -1,22 +1,45 @@
 import socket
 import struct
-import LocationInformation
+import GyroInformation
 import time
+import matplotlib.pyplot as plt
+import threading
+import traceback
 
 
 def main():
+    results = []
+    gui_thread = threading.Thread(target=listeing_function, args=(results,))
+    gui_thread.start()
+    i = 0
+
+    plt.axis([0, 80, -5, 5])
+    plt.ion()
+    last_scat = None
+    print "Listening"
+    while True:
+
+        i += 1
+        # TODO: Remove non relevant results
+        if last_scat:
+            last_scat.remove()
+        last_scat = plt.scatter(range(len(results[-80:])), [result.x for result in results[-80:]])
+        plt.pause(0.001)
+
+
+def listeing_function(results):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind(("0.0.0.0", 15170))
-    print "Listening"
     while True:
         pack = sock.recv(10000)
         location_info = parse_packet(pack)
-        print "(%s: %s,%s,%s)" % (time.ctime(), location_info.x, location_info.y, location_info.z)
+        print "(%s: %s,%s,%s)" % (time.ctime(), location_info.z, location_info.y, location_info.z)
+        results.append(location_info)
 
 
 def parse_packet(packet):
     (x, y, z) = struct.unpack("!fff", packet)
-    return LocationInformation.LocationInformation(x, y, z)
+    return GyroInformation.GyroInformation(x, y, z)
 
 
 if __name__ == "__main__":
