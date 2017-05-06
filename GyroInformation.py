bae9d950fc520cc17b3ba53b9dd3dcf1d52d1926
Ziv <ziv.g22@gmail.com>
2017-05-06 18:34:56 +0300

    Added a calculation for acceleration to velocity

--- GyroInformation.py
+++ GyroInformation.py
@@ -1,5 +1,41 @@
 class GyroInformation(object):
     def __init__(self, x, y, z):
         self.x = x
+        self.x_delay = 0
         self.y = y
+        self.y_delay = 0
         self.z = z
+        self.z_delay = 0
+
+    def add(self, other):
+        self.x += other.x
+        self.y += other.y
+        self.z += other.z
+        
+    def zeroize(self):
+        self.x = 0
+        self.y = 0
+        self.z = 0
+
+    def conditional_add(self, other, threshold):
+        if abs(other.x) > threshold:
+            self.x += other.x
+        else:
+            self.x_delay += 1
+            if self.x_delay >= 10:
+                self.x = 0
+                self.x_delay = 0
+        if abs(other.y) > threshold:
+            self.y += other.y
+        else:
+            self.y_delay += 1
+            if self.y_delay >= 10:
+                self.y = 0
+                self.y_delay = 0
+        if abs(other.z) > threshold:
+            self.z += other.z
+        else:
+            self.z_delay += 1
+            if self.z_delay >= 10:
+                self.z = 0
+                self.z_delay = 0
--- GyroServerGraph.py
+++ GyroServerGraph.py
@@ -9,11 +9,11 @@ import traceback
 
 def main():
     results = []
-    gui_thread = threading.Thread(target=listeing_function, args=(results,))
+    gui_thread = threading.Thread(target=listening_function, args=(results,))
     gui_thread.start()
     i = 0
 
-    plt.axis([0, 80, -5, 5])
+    plt.axis([0, 80, -50, 50])
     plt.ion()
     should_clear_scats = False
     last_x_scat = None
@@ -52,19 +52,32 @@ def main():
             should_clear_scats = True
 
 
-def listeing_function(results):
+def listening_function(results):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind(("0.0.0.0", 15170))
+    velocity_info = GyroInformation.GyroInformation(0,0,0)
+    # last_time = time.ctime()
+    curr_delay = 0.1
     while True:
         pack = sock.recv(10000)
-        location_info = parse_packet(pack)
-        print "(%s: %s,%s,%s)" % (time.ctime(), location_info.z, location_info.y, location_info.z)
-        results.append(location_info)
+        # curr_delay = time.ctime() - last_time
+        location_info, location_info_velocity = parse_packet(pack)
+        if curr_delay < 100:
+            velocity_info.conditional_add(location_info, 1)
+        else:
+            velocity_info.x = 0
+            velocity_info.y = 0
+            velocity_info.z = 0
+            
+        show_info = velocity_info 
+        print "(%s: %s,%s,%s)" % (time.ctime(), show_info.x, show_info.y, show_info.z)
+        results.append(show_info)
 
 
 def parse_packet(packet):
-    (x, y, z) = struct.unpack("!fff", packet)
-    return GyroInformation.GyroInformation(x, y, z)
+    (x, y, z, v_x, v_y, v_z) = struct.unpack("!ffffff", packet)
+    # (x, y, z) = struct.unpack("!fff", packet); v_x = 0; v_y = 0; v_z = 0
+    return GyroInformation.GyroInformation(x, y, z), GyroInformation.GyroInformation(v_x, v_y, v_z)
 
 
 if __name__ == "__main__":
